c5d09c67b687a3a14f8bd2047e8ba890252f74c1
김강년 <64246800+KangNyeonKim@users.noreply.github.com>
2022-09-08 18:29:00 +0900

    webpack 설치 및 환경설정

---
+++ myapp/public/javascripts/mainservice.js
@@ -0,0 +1,15 @@
+export default class MainService {
+  constructor({ targetEl }) {
+    this.targetEl = targetEl;
+    this.dataList = "";
+  }
+
+  init(dataList) {
+    this.dataList = dataList.flat(2).join("");
+    return this.render(this.dataList);
+  }
+
+  render(data) {
+    return `<span>${data}</span>`;
+  }
+}
---
+++ myapp/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./bin/www",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html", //html  webpack플러그인을 통해 html 파일도 함께 bundle
+    }),
+    new MiniCssExtractPlugin({
+      // style 태그 대신 css 파일도 함께 bundle
+      filename: "style.css",
+      chunkFilename: "style.css",
+    }),
+  ],
+};
